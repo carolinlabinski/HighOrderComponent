e8f352c2e9835994253b08c92809f2f68e825e81
carolin.labinski <carolin.labinski@yahoo.de>
2020-05-12 14:13:34 +0200

    correction path

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+// import TotoContext from "components/Toto";
+// import React from "react";
+// import Child from "components/Child";
+
+// const App = () => {
+// 	//const TotoContext = createContext({ name1: "Matthieu", name2: "Mathis" });
+
+// 	return (
+// 		<div>
+// 			<TotoContext.Provider value={{ name1: "Matthieu", name2: "Mathis" }}>
+// 				{" "}
+// 				<Child />
+// 			</TotoContext.Provider>
+// 		</div>
+// 	);
+// };
+
+// export default App;
+
 import React from "react";
 import TotoContext from "./Toto";
-//import Child from "components/Child.jsx";
-import Child from "components/Child";
+import Child from "components/Child/Child.jsx";
+//import Child from "components/Child; avec un dossier Child
 
 const App = () => {
 	return (
--- src/components/Child/index.js
+++
@@ -1,6 +0,0 @@
-import Child from "./Child";
-
-export default Child;
-
-// import { withToto } from "./Child";
-// export default withToto(Child);
